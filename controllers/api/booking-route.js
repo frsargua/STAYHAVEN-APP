3a0a8640adb3f501067045308b7157cf0883f397
frsargua <frsargua@gmail.com>
2022-07-13 17:31:03 +0100

    booking-route for creating a new booking

--- controllers/api/booking-route.js
+++ controllers/api/booking-route.js
@@ -1,5 +1,7 @@
-// const router = require('express').Router();
-// const { User, Property } = require('../../models');
+const router = require('express').Router();
+const { Booking, User, Property } = require('../../models');
+
+console.log(User, Property);
 
 // router.get('/', (req, res) => {
 //   // find all users in the data base
@@ -12,9 +14,15 @@
 //   //TO DO: We will have to pair a User to a table containing its images
 // });
 
-// router.post('/', (req, res) => {
-//   // create a new User
-// });
+router.post('/', async (req, res) => {
+  // create a new User
+  try {
+    const newBooking = await Booking.create(req.body);
+    res.status(200).json(newBooking);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
 
 // router.put('/:id', (req, res) => {
 //   // update a User's description by its `id` value
@@ -24,4 +32,4 @@
 //   // delete a Userby its `id` value
 // });
 
-// module.exports = router;
+module.exports = router;

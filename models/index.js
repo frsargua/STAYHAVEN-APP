7642b1791432f3b93d8b8f076319439f2dc82018
frsargua <frsargua@gmail.com>
2022-07-13 18:03:21 +0100

    added model relationships

--- models/index.js
+++ models/index.js
@@ -4,18 +4,25 @@ const Property = require('./Property');
 const User = require('./User');
 const Bookmark = require('./Bookmark');
 
-// Categories have many Products
-// User.hasMany(Property, {
-//   onDelete: 'CASCADE',
-// });
+// One User can have many properties
+User.hasMany(Property, {
+  onDelete: 'CASCADE',
+});
 
-// Products belongsTo Category
-// Property.belongsTo(User);
+// One property belongs to one user only.
+Property.belongsTo(User);
+
+// One User can have many bookmarks
+User.hasMany(Bookmark, {
+  onDelete: 'CASCADE',
+});
+// One bookmark belongs to one user only.
+Bookmark.belongsTo(User);
 
 // Products belongToMany Tags (through ProductTag)
-// Property.belongsToMany(User, { through: Booking, unique: false });
+Property.belongsToMany(User, { through: Booking, unique: false });
 // Tags belongToMany Products (through ProductTag)
-// User.belongsToMany(Property, { through: Booking, unique: false });
+User.belongsToMany(Property, { through: Booking, unique: false });
 
 module.exports = {
   Booking,

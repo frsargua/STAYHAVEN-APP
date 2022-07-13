a10677a7e7da717f58b0c68c93affa81170c78ff
frsargua <frsargua@gmail.com>
2022-07-13 15:57:56 +0100

    added loggedIn bolean when signed in

--- controllers/api/user-route.js
+++ controllers/api/user-route.js
@@ -39,7 +39,11 @@ router.post('/signUp', async (req, res) => {
   console.log(Property);
   try {
     const userData = await User.create(req.body);
-    res.status(200).json(userData);
+    req.session.save(() => {
+      req.session.loggedIn = true;
+
+      res.status(200).json(userData);
+    });
   } catch (err) {
     res.status(400).json(err);
   }

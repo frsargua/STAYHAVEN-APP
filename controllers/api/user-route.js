9c31264da8a8b81cf3b5fffc4e32b1aa2baf6a5e
frsargua <frsargua@gmail.com>
2022-07-13 14:57:37 +0100

    GET - Get user by id

--- controllers/api/user-route.js
+++ controllers/api/user-route.js
@@ -9,9 +9,23 @@ const { User, Property } = require('../../models');
 //   // find all users rentals in the database
 // });
 
-// router.get('/:id', (req, res) => {
-//   // find one User by its `id` value
-// });
+router.get('/:id', async (req, res) => {
+  // find one User by its `id` value
+  try {
+    const userByID = await User.findOne({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!userByID) {
+      res.status(404).json({ message: 'Id does not exist' });
+      return;
+    }
+    res.status(200).json(userByID);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 // router.post('/', (req, res) => {
 //   // create a new User

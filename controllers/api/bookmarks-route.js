64571c7b75ed6506b8090d295f01d21dc3b5fe0d
frsargua <frsargua@gmail.com>
2022-07-13 16:59:08 +0100

    added bookmarks route and created a post request

--- controllers/api/bookmarks-route.js
+++ controllers/api/bookmarks-route.js
@@ -1,15 +1,21 @@
 const router = require('express').Router();
-// const { Bookmark, User, Property } = require('../../models');
+const { Bookmark, User, Property } = require('../../models');
 
+console.log(User, Property);
 // router.get('/', (req, res) => {
 //   //will use session storage to find the properties associated to the user. (once logged in)
 //   // find all bookmarks in the data base
 //   // be sure to include its associated Users and property
 // });
 
-// router.post('/', (req, res) => {
-//   // create a new bookmark
-// });
+router.post('/', async (req, res) => {
+  try {
+    const newBookmark = await Bookmark.create(req.body);
+    res.status(200).json(newBookmark);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
 
 // router.delete('/:id', (req, res) => {
 //   // delete a bookmark

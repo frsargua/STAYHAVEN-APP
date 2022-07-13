96c091fb264713bf48194f3e753b927b21b7d2f2
frsargua <frsargua@gmail.com>
2022-07-13 11:40:41 +0100

    not found page

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const homeRoutes = require('./homeRoutes');
 
-router.use('/', homeRoutes);
-
+router.use('/api', homeRoutes);
+router.use((req, res) => {
+  res.send('<h1>Wrong Route!</h1>');
+});
 module.exports = router;

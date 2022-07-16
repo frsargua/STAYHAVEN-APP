7a97f5409ec6c84afbcb77caa2f1f61c01bcd7be
frsargua <frsargua@gmail.com>
2022-07-16 22:09:00 +0100

    created homepage and decscriptionpage handlebars files, and their js routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,5 +3,8 @@ const router = require('express').Router();
 router.get('/', async (req, res) => {
   res.render('homepage');
 });
+router.get('/about-property', async (req, res) => {
+  res.render('descriptionpage');
+});
 
 module.exports = router;

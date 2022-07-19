7c8a8960f5028092515489e47bf23eca7799fe82
frsargua <frsargua@gmail.com>
2022-07-19 15:21:36 +0100

    added profile page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -66,6 +66,19 @@ router.get('/add-listing', withAuth, async (req, res) => {
   res.render('addListing', { logged });
 });
 
+router.get('/user-profile/:id', async (req, res) => {
+  let logged = req.session.logged_in;
+  let user = await User.findOne({
+    raw: true,
+    attributes: { exclude: ['password'] },
+    where: {
+      id: req.params.id,
+    },
+  });
+
+  res.render('profile', { logged, user });
+});
+
 router.get('/login', async (req, res) => {
   let logged = req.session.logged_in;
   res.render('loginPage', { logged });

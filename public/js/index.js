45aeac87d28cf30a88ac0f40371c7224dc1a38bc
frsargua <frsargua@gmail.com>
2022-08-04 14:26:22 +0100

    refactore using form to create new property

--- public/js/index.js
+++ public/js/index.js
@@ -198,41 +198,11 @@ if (window.location.pathname.includes('/about-property/')) {
   });
 }
 
-//Posting the data to the db
-if (window.location.pathname === '/add-listing') {
-  autocompleteFunc();
-}
-
 async function exchange() {
   let propertyAvailability = $('#flexSwitchCheckChecked');
   let available = propertyAvailability.prop('checked');
-  let value = propertyAvailability.prop('value', available).prop('value');
+  propertyAvailability.prop('value', available).prop('value');
 }
-
-// async function createNewProperty() {
-//   const newPropertyForm = document.getElementById('newProperty');
-//   newPropertyForm.addEventListener('click', async (e) => {
-//     e.preventDefault();
-
-//     const newPropertyFormFields = new FormData(newPropertyForm);
-//     const formProps = Object.fromEntries(newPropertyFormFields);
-//     let propertyAvailability = $('#flexSwitchCheckChecked')
-//       .prop('checked')
-//       .prop('value', true);
-//     formProps.available = propertyAvailability;
-
-//     console.log(formProps);
-
-//     // const response = await fetch('/api/property/', {
-//     //   method: 'POST',
-//     //   body: JSON.stringify(formProps),
-//     //   headers: { 'Content-Type': 'application/json' },
-//     // });
-//     // if (response.ok) {
-//     //   window.location.href = '/';
-//     // }
-//   });
-// }
 
 // Sign Out
 async function signOut() {
